f0c5942d833cefc903514196e63a3c19ba2363c3
Brian Matheny <bwmatheny@gmail.com>
2020-05-21 14:57:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,11 +7,11 @@ def key_for_min_value(name_hash)
 
   name_hash.each do |name, value|
     if min_value = nil
-     min_value = 0 
+     min_value = 0
     elsif value < min_value
       min_value = value
       min_key = name
-      
+
     end
   end
   min_key
